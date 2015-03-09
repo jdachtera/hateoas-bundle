d4723431a3c707a1add89a1454561098b97b9250
Jascha Dachtera <jascha.dachtera@gmail.com>
2015-03-09 15:16:15 +0100

    Fix test autoload configuration

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,3 +1,14 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('UTC');
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    // dependencies were installed via composer - this is the main project
+    require __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    // installed as a dependency in `vendor`
+    require __DIR__ . '/../../../autoload.php';
+} else {
+    throw new \Exception('Can\'t find autoload.php. Did you install dependencies via composer?');
+}
